65e4f3eca383d8b8a62c571bc5cb3feb52bdc389
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-03-27 15:38:46 -0600

    Add carved rock query

---
+++ GraphQLCore/GraphQL/CarvedRockQuery.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+using GraphQLCore.GraphQL.Types;
+using GraphQLCore.Repositories;
+
+namespace GraphQLCore.GraphQL
+{
+    public class CarvedRockQuery : ObjectGraphType
+    {
+        public CarvedRockQuery(ProductRepository productRepository)
+        {
+            Field<ListGraphType<ProductType>>("products", resolve: context => productRepository.GetAll());
+        }
+    }
+}
