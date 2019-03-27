ead0c68040bf33785e85b3bdd710582955081ec1
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-03-27 15:42:07 -0600

    Register GraphQL middleware

--- GraphQLCore/Startup.cs
+++ GraphQLCore/Startup.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
 using GraphQLCore.Data;
+using GraphQLCore.GraphQL;
 using GraphQLCore.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -26,18 +30,26 @@ namespace GraphQLCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddDbContext<CarvedRockDbContext>(options =>
             {
                 options.UseSqlServer(_config["ConnectionStrings:CarvedRock"]);
             });
             services.AddSingleton<ProductRepository>();
+
+            services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+            services.AddScoped<CarvedRockSchema>();
+            services.AddGraphQL(o =>
+            {
+                o.ExposeExceptions = true;
+            })
+            .AddGraphTypes(ServiceLifetime.Scoped);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, CarvedRockDbContext dbContext)
         {
-            app.UseMvc();
+            app.UseGraphQL<CarvedRockSchema>();
+            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
             dbContext.Seed();
         }
     }
