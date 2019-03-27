9a3356221056151150c8756caa7f69a6b2f3841d
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-03-27 15:32:36 -0600

    Add dbcontext

---
+++ GraphQLCore/Data/CarvedRockDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GraphQLCore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLCore.Data
+{
+    public class CarvedRockDbContext: DbContext
+    {
+        public CarvedRockDbContext(DbContextOptions<CarvedRockDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
