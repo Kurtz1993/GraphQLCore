1de3d00e3f97b98f97c35dafb3c1110c5ed95e3d
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-03-27 15:32:53 -0600

    Add repository for products

---
+++ GraphQLCore/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GraphQLCore.Data;
+using GraphQLCore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLCore.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly CarvedRockDbContext _dbContext;
+
+        public ProductRepository(CarvedRockDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<Product>> GetAll()
+        {
+            return _dbContext.Products.ToListAsync();
+        }
+    }
+}
